3b74818ec057820feeee42b98310abafe908b209
Hadeer Esam <hadeeresamcom33@gmail.com>
2020-06-11 19:14:29 +0200

    animation of side navbar

---
+++ js/animation.js
@@ -0,0 +1,29 @@
+
+let activeSection;
+
+// fade in choosen setting section
+$(".menue .nav-link").click(function () { 
+    let href=$(this).attr("href");
+    activeSection=href;
+    $(href).fadeIn(700);
+    $(href).siblings().css("display","none");
+});
+
+//show section according to size of browser
+$(window).resize(function () {
+    let bodyWidth=$("html,body").outerWidth();
+    // console.log(bodyWidth)
+    if(bodyWidth>750){
+
+        //show the choosen section before and hide the others
+        $(activeSection).css("display","block");    
+        $(activeSection).siblings().css("display","none");
+    }
+    else{
+        //show all sections in mobile size
+        $(".main-settings").children().css("display","block");
+    }
+});
+
+
+

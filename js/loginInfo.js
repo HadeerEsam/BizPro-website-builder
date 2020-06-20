bad605561eee78b4ed7184687d0362d43fa23160
Hadeer Esam <hadeeresamcom33@gmail.com>
2020-06-20 16:28:54 +0200

    delete js code related to home and sign up pages,adding trim for user name

--- js/loginInfo.js
+++ js/loginInfo.js
@@ -13,53 +13,6 @@ if(localStorage.getItem("users") == null&& localStorage.getItem("numberOfUsers")
     users = JSON.parse(localStorage.getItem("users"));
 }
 
-$(".startnow").attr("href" , "login.html" );
-
-// welcome page
-
-
-
-$(window).scroll(function () {
-
-    let stunningFromTop = $(".stunning-templates").offset().top;
-    let dragAndDrobFromTop = $(".drag-drop").offset().top;
-    let MobileFromTop = $(".mobile").offset().top;
-    let distanceFromTop = $(window).scrollTop();
-
-    if(distanceFromTop >= stunningFromTop - 400){
-
-        $(".stunning-templates h3").animate({marginLeft : "0px"} , 1300);
-        $(".stunning-templates p").animate({marginLeft: "0px"} , 1600);
-        $(".stunning-templates a").animate({marginLeft: "0px"} , 1900);
-        $(".stunning-templates img").fadeIn(1900);
-
-    }
-
-    if(distanceFromTop >= dragAndDrobFromTop - 400){
-
-        $(".drag-drop h3").animate({marginLeft : "0px"} , 1300);
-        $(".drag-drop p").animate({marginLeft: "0px"} , 1600);
-        $(".drag-drop a").animate({marginLeft: "0px"} , 1900);
-        $(".drag-drop img").fadeIn(2000);
-
-    }
-
-    if(distanceFromTop >= MobileFromTop - 400){
-
-        $(".mobile h3").animate({marginLeft : "0px"} , 1300);
-        $(".mobile p").animate({marginLeft: "0px"} , 1600);
-        $(".mobile a").animate({marginLeft: "0px"} , 1900);
-        $(".mobile img").fadeIn(2000);
-
-    }
-
-})
-
-
-
-// end of welcome page
-
-
 // start of login page code
 
  $("#login").click(function(){
@@ -70,7 +23,7 @@ $(window).scroll(function () {
 
     for(let i = 0; i < users.length; i++){
 
-        if(users[i].userName == $("#username-input").val()){
+        if(users[i].userName == $("#username-input").val().trim()){
 
             indexOfRight = i;
             rightUsername = true;
@@ -115,106 +68,5 @@ $(window).scroll(function () {
 })
 
 // end of login page code
-
-
-// start of registration page code
-
-$("#signup-btn").click(function () { 
-
-    let exist = false;
-
-    //checking if the username is already existed
-
-    for(let i = 0; i < users.length; i++){
-
-        if(users[i].userName == $("#username").val())
-        {
-            exist = true;
-            break;
-        }
-
-    }
-
-    if(exist == false){
-
-        //does not exist
-
-        // to hide exist paragraph if appeared
-        $("#existed").css("display" , "none");
-
-        if($("#password").val() == $("#repeated-password").val()){
-
-            // to hide missmatch paragraph if appeared
-            $("#password-missmatch").css("display" , "none");
-            addUser();
-            
-            numberOfUsers=users.length-1;
-           localStorage.setItem("numberOfUsers",JSON.stringify(numberOfUsers));                      
-
-           $("#sign-up").attr("href","mySites.html");
-
-
-        }else{
-
-            // to show missmatch paragraph
-            $("#password-missmatch").css("display" , "block");
-
-        }
-
-    }else{
-
-        //exists
-        $("#existed").css("display" , "block");
-
-    }
-});
-
-
-function addUser() {
-
-    let userInfo = {
-
-        userName : $("#username").val() ,
-        userEmail : $("#email").val() ,
-        userPassword :$("#password").val()
-
-    }
-    
-    users.push(userInfo);
-    localStorage.setItem("users" , JSON.stringify(users));
-}
-
-
-// end of registration page code
-
-
-//start of account setting page
-let enteredUser;
-function userInfo(){
-     enteredUser=Number(localStorage.getItem("numberOfUsers"));
-    $("#userName-settings").val(users[enteredUser].userName);
-    $("#email-settings").val(users[enteredUser].userEmail);
-    $("#password-settings").val(users[enteredUser].userPassword);
-    $("#Name").text(users[enteredUser].userName);
-    $("#userEmail").text(users[enteredUser].userEmail);
-}
-userInfo();
-
-$("#updateBtn").click(function () { 
-    users[enteredUser].userName= $("#userName-settings").val();
-    users[enteredUser].userEmail= $("#email-settings").val();
-    users[enteredUser].userPassword=$("#password-settings").val();
-    localStorage.setItem("users" , JSON.stringify(users));
-});
-
-
-
-//end of account setting page
-
-
-
-
-
-
 
 

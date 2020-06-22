de402b8c5ad18f0b34adcaaaa04dd7e4b6500190
Hadeer Esam <hadeeresamcom33@gmail.com>
2020-06-22 02:21:35 +0200

    deleting side navs js code, adding show and hide of documentation page

--- js/editorscript.js
+++ js/editorscript.js
@@ -1,76 +1,12 @@
 $(document).ready(function(){
-//toggle of tools taps
-$("#rightNav .nav-link").click(function () { 
-    $(this).addClass("active-option");
-    $("#rightNav .nav-link").not($(this)).removeClass("active-option");
-    let href=$(this).attr("href");
-    $(`${href}`).addClass("show");
-    $(`${href}`).siblings().removeClass("show");
- });
 
- $(".elem .nav-link").click(function () { 
-    $(this).addClass("active-option");
-    $(".elem .nav-link").not($(this)).removeClass("active-option");
-    let href=$(this).attr("href");
-    $(`${href}`).addClass("show");
-    $(`${href}`).siblings().removeClass("show");
- });
-
- //hide side navs
- $("#hideRightNav").click(function(){
-    if($("#main-section").css("width")<"80%"){
-        if( $("#rightNav").css("right")=="0px"){
-            $("#main-section").css("right","0");
-            $("#rightNav").css("right","-20%");
-            
-         }else{
-            $("#main-section").css("right","20%");
-            $("#rightNav").css("right","0");
-         }
-    }else{
-        if( $("#rightNav").css("right")=="0px"){
-            $("#main-section").css({"right":"0","width":"auto"});
-            $("#rightNav").css("right","-20%");
-            
-         }else{
-            
-            $("#rightNav").css("right","0");
-            $("#main-section").css({"right":"20%","width":"auto"});
-         }
-
-    }
- });
-
- 
- $("#hideLeftNav").click(function(){
-    if($("#main-section").css("width")<"80%"){
-        if( $("#left-side").css("left")=="0px"){
-            $("#main-section").css("left","0");
-            $("#left-side").css("left","-20%");
-         }else{
-            $("#main-section").css("left","20%");
-            $("#left-side").css("left","0");
-         }
-    }else{
-        if( $("#rightNav").css("right")=="0px"){
-            $("#main-section").css({"left":"0","width":"auto"});
-            $("#left-side").css("left","-20%");
-            
-         }else{
-            
-            $("#left-side").css("left","0");
-            $("#main-section").css({"left":"20%","width":"auto"});
-         }
-
-    }
-    
-});
 
 //show responsive website inside editor
+//mobile size
 $("#mobile").click(function(){
     $("#main-section").css("width","320px");
 });
-
+//tablet size
 $("#tablet").click(function(){
     $("#main-section").css("width","768px");
 });
@@ -129,7 +65,7 @@ function IsFullScreenCurrently() {
 	else
 		return true;
 }
-
+//full screen button function
 $("#fullscreen").click(function(){
     if(IsFullScreenCurrently())
         GoOutFullscreen();
@@ -138,19 +74,20 @@ $("#fullscreen").click(function(){
 });
 
 
-// ///////////////// sidebar templates //////////////////
+// ///////////////// dropDown templates //////////////////
 
+    ///////   jumbotron      ///////
     $("#firstTemplate").click(function(){
       $('iframe').attr('src', "jumbotron.html")
     
     });
 
-    ///////   landing page    //////
+    ///////   landing page    
     $(".landing").click(function(){
         $('iframe').attr('src', "template.html")
     });
 
-    // ////   albums page ///////
+    // ////   albums page 
     $("#album-template").click(function(){
         $('iframe').attr('src', "content.html");
     });  
@@ -167,7 +104,7 @@ $("#fullscreen").click(function(){
         $("html,body").animate({scrollTop:"0"} , 2000);
     });
 
-   
+   // color active template name
     $(".items a").click(function(){
         $(this).addClass("active-page");
         $(".items a").not($(this)).removeClass("active-page");
@@ -175,38 +112,14 @@ $("#fullscreen").click(function(){
     });
     
        
-        // $("#ifrm").ready(function(){
-        //     let ifrm = document.getElementById('ifrm');
-        //         console.log(ifrm)
-        //         let win = ifrm.contentWindow; 
-        //     // reference to iframe's window
-        //     // reference to document in iframe
-        //     let doc = ifrm.contentDocument? ifrm.contentDocument: ifrm.contentWindow.document;
-        //    $("p").click(function(){
-        //     $("#contentTap").html(`
-        //     <div class="text-align">
-        //     <div><i class="fas fa-align-left"></i></div>
-        //     <div><i class="fas fa-align-justify"></i></div>
-        //     <div><i class="fas fa-align-right"></i></div>
-        //     </div>
-        //     <div></div>
-        //     `);    
-        //    });    
-        // });
-        
-        // doc.innerHTML.addEventListner("click",function(){
-        //     $(this).css("border","1px solid #d73e4d");
-        //     $("p").not($(this)).css("border","none");
-        // // $(this).contents().find('body').html('Hey, I have changed the body content yay!');
+        // documentation of editor 
 
-    
-            
-        // });
-
-        // $("iframe .nav-link").click(function(){
-        //     window.alert("sd")
-
-        // });
+        $("#help").click(function(){
+            $("#support").css("display","block");
+        });
+        $("#close").click(function(){
+            $("#support").css("display","none");
+        });
     });
     
 
